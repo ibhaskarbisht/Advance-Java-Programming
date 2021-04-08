b964829e17a16ed504c97c57c7f674e7a01c784d
ibhaskarbisht <bhaskarbisht201@gmail.com>
2021-04-08 15:45:21 +0530

    Practical 5

---
+++ Pratical - 5 (Server sends url and host print the host address)/Server.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+public class Server
+{
+	public static void main(String[]args) throws Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		DatagramSocket ds= new DatagramSocket();
+		InetAddress ip=InetAddress.getLocalHost();
+		byte buf[]=null;
+		String inp=sc.nextLine();
+		buf=inp.getBytes();
+		DatagramPacket dpsend=new DatagramPacket(buf,buf.length,ip,1234);
+		ds.send(dpsend);
+	}
+}
+
