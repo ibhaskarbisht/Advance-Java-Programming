fbfd2efe9b6479ce1531d5d865a98418fdc18c94
ibhaskarbisht <bhaskarbisht201@gmail.com>
2021-05-18 19:37:56 +0530

    Practical 11

---
+++ Practical 11/Result.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="p" uri="/struts-tags"  %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Sum Result</title>
+</head>
+<body>
+	Sum of <p:property value="x"/>
+	and <p:property value="y"/>
+	is:
+	<p:property value="sum"/>
+</body>
+</html>
---
+++ Practical 11/Sum.java
@@ -0,0 +1,40 @@
+package net.codejava.struts;
+
+ import com.opensymphony.xwork2.ActionSupport;
+ 
+ public class SumAction extends ActionSupport{
+	 private int x;
+	 private int y;
+	 private int sum;
+	 
+	 public String calculate() {
+		 sum = x+y;
+		 return SUCCESS;
+		 
+	 }
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	 
+ }
